39aee841e3a2def5d2c2406dfcc3940484d1ccda
Moonhee <o119a@naver.com>
2022-04-17 20:03:01 +0900

    updated_반복문

--- BAEKGJOON/반복문.js
+++ BAEKGJOON/반복문.js
@@ -12,3 +12,28 @@ for (let i=1; i < 10; i++){
 }
 
 // 10950 A+B - 3
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `5
+1 1
+2 3
+3 4
+9 8
+5 2
+`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let t = input();
+while(t--){
+    const [a,b] = input().split(' ').map(Number);
+    console.log(a + b)
+};
+
+
+// 10950 A+B - 3
--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 const fs = require('fs');
 const stdin = (process.platform === 'linux'
-  ? fs.readFileSync('/dev/stdin').toString()
-  : `10 10 3
-13 2 5 11 7 8 2 4 9 10
-1
-2
-3
-8
-9
-10
-11
-16
-17
-49
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `3
 `
 ).split('\n');
 
@@ -21,23 +10,10 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-let [n, m, v] = input()
-console.log([n, m, v])
-// let [n, m, v] = input().split(' ').map(Number);
-// const cycle = [], non = [];
-// v--;
-// input()
-//   .split(' ')
-//   .forEach((x, i) => {
-//     x = +x;
-//     if (i < v) non.push(x);
-//     else cycle.push(x);
-//   });
+let t = input();
+for (let i = 1; i <= t; i++){
+    let total = i;
+    total += i
+    console.log(total)
+}
 
-// let ans = [];
-// for (let i = 0; i < m; i++) {
-//   const k = +input();
-//   if (k < v) ans.push(non[k]);
-//   else ans.push(cycle[(k - v) % (n - v)]);
-// }
-// console.log(ans.join('\n')); 
