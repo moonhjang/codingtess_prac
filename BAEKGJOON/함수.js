3b912897ee9ff50d24e291d8bf61b7517de738bf
Moonhee <o119a@naver.com>
2022-05-02 08:54:43 +0900

    함수 완료

--- BAEKGJOON/함수.js
+++ BAEKGJOON/함수.js
@@ -1,5 +1,24 @@
 // 4673 셀프 넘버
+const list1 = [1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97]; 
+const total = []; 
+const list = []; 
 
+for (let i = 1; i < 10000; i++){
+  total.push(i)
+}
+
+for (let i = 1; i < 10000; i++){
+  const num = (i+'').split('').map(Number)
+  const sum = num.reduce((prev, curr) => prev + curr )
+  const createNum = i + sum
+  if (createNum < 10000){
+    list.push(createNum)
+  }
+}
+
+const Num = list.sort((a,b)=> a-b)
+const selfNumber = total.filter(x => !Num.includes(x))
+selfNumber.map(n => console.log(n))
 
 
 
--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 
-// 1~10000이 들어간 리스트만들기 (1)
+const list1 = [1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97]; 
+const total = []; 
+const list = []; 
 
-// for (let i = 1; i < 10000; i++){
-  i = 9999
+for (let i = 1; i < 10000; i++){
+  total.push(i)
+}
+
+for (let i = 1; i < 10000; i++){
   const num = (i+'').split('').map(Number)
-
-  console.log(num)
-
-  // const numList = [Number(num)+'']
-  // console.log(numList)
   const sum = num.reduce((prev, curr) => prev + curr )
-  // console.log(a)
   const createNum = i + sum
-  console.log(createNum)
+  if (createNum < 10000){
+    list.push(createNum)
+  }
+}
 
-  // 리스트에서 create Num 삭제하기 (2)
-
-    if (i === createNum)
-
-
-
-
-
-// }
-
-//(3) 리스트를 콘솔로 내보내기
+const Num = list.sort((a,b)=> a-b)
+const selfNumber = total.filter(x => !Num.includes(x))
+selfNumber.map(n => console.log(n))
