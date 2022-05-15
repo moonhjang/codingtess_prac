7274f91140e85fe97849ac0b17f568a59cea27af
Moonhee <o119a@naver.com>
2022-05-16 08:19:00 +0900

    기본수학 작업

---
+++ BAEKGJOON/07_기본수학 1.js
@@ -0,0 +1,169 @@
+// 1712 손익분기점
+// Math.Floor를 알아야 하는 문제!
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `0 60 70`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line];
+})();
+
+const t = input().split(' ').map(Number)
+
+const A = t[0]
+const B = t[1]
+const T = t[2]
+
+
+if ( B > T || B === T ){
+  console.log(-1)
+} 
+
+else {
+  const margin = T - B
+  const answer = Math.floor ( A / margin ) +1
+  console.log(answer)
+}
+
+// 2292 벌집
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `66`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line];
+})();
+
+const t = Number(input())
+
+let range= 1;
+
+if (t === 1){
+  console.log(1)
+}
+
+for (let i=1; i < t; i++){
+  const line = 6 * i
+  range += line
+
+  if ( t <= range) {
+    console.log(i+1)
+    break;
+  }
+}
+
+// 1193 분수찾기
+// 이것 진짜 너무 어려움. (다른사람것)
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `14`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line];
+})();
+
+let t = Number(input())
+
+let groupCounter = 0;
+let ascending = [];
+let descending = [];
+
+while ( t > 0 ) {
+  groupCounter++;
+  t = t - groupCounter
+}
+
+for (let i= 0; i < groupCounter; i++){
+  ascending.push(i+1);
+  descending.push(groupCounter-i)
+}
+
+if(groupCounter %2 === 0) {
+  console.log(`${ascending[groupCounter-1+t]}/${
+    descending[groupCounter-1+t]
+  }`);
+} else {
+  console.log(`${descending[groupCounter-1+t]}/${
+    ascending[groupCounter-1+t]
+  }`);
+}
+
+
+// 2869 달팽이는 올라가고 싶다
+// 시간 초과나옴
+// const fs = require('fs');
+// const stdin = (process.platform === 'linux'
+//     ? fs.readFileSync('/dev/stdin').toString()
+//     : `2 1 5`
+// ).split('\n');
+
+// const input = (() => {
+//   let line = 0;
+//   return () => stdin[line];
+// })();
+
+// const t = input().split(' ').map(Number)
+// let arrive = 0;
+
+// for(let i=1; i<=t[2]; i++){
+//   let doDay = t[0]
+//   arrive += doDay;
+//   if(arrive < t[2]){
+//     arrive -= t[1]
+//   } else 
+//   if(arrive === t[2] || arrive > t[2]){  
+//     arrive += doDay
+//     console.log(i)    
+//     break;
+//   } 
+// }
+
+// 2869 달팽이는 올라가고 싶다 (찾은내용)
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `2 1 5`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line];
+})();
+
+const t = input().split(' ').map(Number)
+// console.log(t)
+const A = t[0];
+const B = t[1];
+const V = t[2];
+ 
+console.log(Math.ceil((V - B) / (A - B)));
+
+
+// 2839 설탕배달
+
+// 10757 큰수 A+B 
+// BigInt()를 알았어야 하는문제
+const fs = require('fs');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `9223372036854775807 9223372036854775808`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line];
+})();
+
+const t = input().split(' ').map(BigInt)
+
+const Int = t[0] + t[1]
+console.log(Int.toString())
--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+// const fs = require('fs');
+// const stdin = (process.platform === 'linux'
+//     ? fs.readFileSync('/dev/stdin').toString()
+//     : `14`
+// ).split('\n');
 
+// const input = (() => {
+//   let line = 0;
+//   return () => stdin[line];
+// })();
 
+// let t = Number(input())
 
+// let groupCounter = 0;
+// let ascending = [];
+// let descending = [];
 
+// while ( t > 0 ) {
+//   groupCounter++;
+//   t = t - groupCounter
+// }
 
+// for (let i= 0; i < groupCounter; i++){
+//   ascending.push(i+1);
+//   descending.push(groupCounter-i)
+// }
 
+// if(groupCounter %2 === 0) {
+//   console.log(`${ascending[groupCounter-1+t]}/${
+//     descending[groupCounter-1+t]
+//   }`);
+// } else {
+//   console.log(`${descending[groupCounter-1+t]}/${
+//     ascending[groupCounter-1+t]
+//   }`);
+// }
 
 
 
@@ -26,43 +56,155 @@
 const fs = require('fs');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `M`
+    : `18`
 ).split('\n');
 
 const input = (() => {
   let line = 0;
-  return () => stdin[line++];
+  return () => stdin[line];
 })();
 
-const t = input().toUpperCase().split('').sort() 
-const findStr= [];
-let count = 0;
+const t = Number(input())
 
-// 글자가 하나인경우
-for (let i=0; i<t.length; i++){
-  if (t.length === 1){                     
-    findStr.push(t[0])
+let pack = [];
+
+
+
+for (let i=1; i<7; i++){
+  for (let j=1; j<7; j++){
+    const delivery = 3*i + 5*j
+    // console.log(typeof delivery)
+    // console.log(typeof t)
+    // console.log(t % 3 )
+    const left3 =  t % 3
+    const left5 =  t % 5
+    console.log(left3 === 0 || left5 === 0, '똑떨어짐.')
+    console.log(left3 % 5 === 0 || left5 % 3 === 0, '그래도 똑떨어짐')
+    console.log(left3 % 5 !== 0 && left5 % 3 !== 0, '안됨.')
+    // console.log(left5 % 3 === 0 )
+    // console.log(left5 % 3 !== 0)
+    // console.log(left5 % 5 !== 0 )
+
+    if ( left3 === 0 || left5 === 0 ){
+      console.log( t === delivery, '똑떨어지나?---1')
+      if( t === delivery) {
+        console.log(1, i,j)
+        console.log(i+j)
+        break;
+      } 
+    } else if (left3 % 5 === 0 || left5 % 3 === 0){
+      console.log( t === delivery, '똑떨어지나?---2')
+      if( t === delivery) {
+        console.log(2, i,j)
+        console.log(i+j)
+        break;
+      } 
+    } else if (left3 % 5 !== 0 && left5 % 3 !== 0){
+      console.log( t === delivery, '똑떨어지나?---3')
+      console.log(3, i,j)
+      console.log(-1)
+      break;
+    } else {
+      console.log(i,j)
+    }
+
   }
 
-  // 앞 글자 또는 전글자가 같은 경우
-  if (t[i] === t[i+1] || t[i] === t[i-1]){  
-      // findStr 배열이 없거나, 가상의 findStr 배열의 글자와 같은 경우
-      if(t[i] === findStr[0]|| findStr.length === 0){
-        // console.log(findStr)
-        findStr.push(t[i])  //현재글자를 넣어줌
-        count += 1
-      } else {
-        // console.log(findStr)
-        findStr.pop()       //배열에 있거나, 글자가 다른경우 가상의 배열을 삭제
-        count -= 1
-          if (findStr.length === 0){
-            break;
-          }
+    if ( left3 === 0 || left3 % 5 === 0 || left5 === 0 || left5 % 3 === 0){
+
+    // if ( left3 === 0 || left5 === 0 ){
+      if( t === delivery) {
+      console.log(i,j)
+      console.log(i+j)
+      break;
       }
+    } else if (left3 % 5 !== 0 && left5 % 3 !== 0) {
+      console.log(-1)
+      break;
+    }
   } 
-}
-// console.log(findStr)
-console.log(findStr.length > 0?  findStr[0] :'?')
+ 
+
+console.log(pack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const stdin = (process.platform === 'linux'
+//     ? fs.readFileSync('/dev/stdin').toString()
+//     : `M`
+// ).split('\n');
+
+// const input = (() => {
+//   let line = 0;
+//   return () => stdin[line++];
+// })();
+
+// const t = input().toUpperCase().split('').sort() 
+// const findStr= [];
+// let count = 0;
+
+// // 글자가 하나인경우
+// for (let i=0; i<t.length; i++){
+//   if (t.length === 1){                     
+//     findStr.push(t[0])
+//   }
+
+//   // 앞 글자 또는 전글자가 같은 경우
+//   if (t[i] === t[i+1] || t[i] === t[i-1]){  
+//       // findStr 배열이 없거나, 가상의 findStr 배열의 글자와 같은 경우
+//       if(t[i] === findStr[0]|| findStr.length === 0){
+//         // console.log(findStr)
+//         findStr.push(t[i])  //현재글자를 넣어줌
+//         count += 1
+//       } else {
+//         // console.log(findStr)
+//         findStr.pop()       //배열에 있거나, 글자가 다른경우 가상의 배열을 삭제
+//         count -= 1
+//           if (findStr.length === 0){
+//             break;
+//           }
+//       }
+//   } 
+// }
+// // console.log(findStr)
+// console.log(findStr.length > 0?  findStr[0] :'?')
 
 
 
