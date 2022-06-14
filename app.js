8de94634e75004aa1074cf97be68f7f4509a77d3
Moonhee <o119a@naver.com>
2022-06-14 09:43:52 +0900

    01_입출력과 사친연산 ing

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const fs = require('fs');
-const { start } = require('repl');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `2 16`
+    : `joonas`
 ).split('\n');
 
 const input = (() => {
   let line = 0;
-  return () => stdin[line];
+  return () => stdin[line++];
 })();
 
 const t = input()
-console.log(t)
 
-'어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다. \n 
-"재귀함수가 뭔가요?"
-"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n,
-마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n,
-그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."\n,
-"재귀함수는 자기 자신을 호출하는 함수라네" 라고 답변하였지';
-
-for (let i=0; i<t.length; i++){
-  console.log(i)
-}
-
+console.log(t + '??!')
