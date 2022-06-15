071fb7f5643fcdfe89071eb610e80d71599e4537
Moonhee <o119a@naver.com>
2022-06-15 10:34:48 +0900

    01_입출력과 사친연산_22944

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-const fs = require('fs');
-const stdin = (process.platform === 'linux'
-    ? fs.readFileSync('/dev/stdin').toString()
-    : `472
-385`
-).split('\n');
-
-const input = (() => {
-  let line = 0;
-  return () => stdin[line++];
-})();
-
-const n1 = input()
-const n2 = input()
-const n2_Each = n2.split('').map(Number)
-
-console.log(n1 * n2_Each[2])
-console.log(n1 * n2_Each[1])
-console.log(n1 * n2_Each[0])
-console.log(n1 * n2)
+console.log(`         ,r\'\"7
+r\`-_   ,\'  ,/
+ \\. \". L_r\'
+   \`~\\/
+      |
+      |`)
