fb0357c3da1b4bc6e0f4ad059fd00e0fdd9e6d72
Moonhee <o119a@naver.com>
2022-06-19 23:53:35 +0900

    04_1차원배열_4344

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
+const { start } = require('repl');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `26`
+    : `5
+5 50 50 70 80 100
+7 100 95 90 80 70 60 50
+3 70 90 80
+3 70 90 81
+9 100 99 98 97 96 95 94 93 91`
 ).split('\n');
 
 const input = (() => {
@@ -9,24 +15,16 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-const t = Number(input())
-let num = t
-let sum;
-let i = 0
+const t = input()
 
-while (true){
-  i++;
+for(let i=0; i<t; i++){
+  const a = input().split(' ').map(Number)
 
-  sum = Math.floor(num/ 10) + num% 10;
-  console.log(i, Math.floor(num/ 10), num% 10)
-
-  console.log(i, (num % 10)* 10, sum % 10, num, sum)
-  num = (num % 10) * 10 + sum % 10;
-  // console.log(i, (num % 10)* 10, sum % 10, num)
-
-  if (t === num){
-    break;
+  let total = 0
+  for (let j=1; j<= a[0]; j++){
+    total += a[j]
   }
+  const b = a.filter(num => num = num > total/a[0])
+  const answer = (b.length/a[0]*100).toFixed(3)+'%'
+  console.log(answer)
 }
-
-console.log(i)
