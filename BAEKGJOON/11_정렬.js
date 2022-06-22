d1b0a9200a68e4601fee7ed46ef6d36e7fa5682e
Moonhee <o119a@naver.com>
2022-06-22 10:04:30 +0900

    11_정렬_1427

--- BAEKGJOON/11_정렬.js
+++ BAEKGJOON/11_정렬.js
@@ -1,13 +1,13 @@
-// 2750 수 정렬하기 (다시 확인할 것)
+// 2750 수 정렬하기
 const fs = require('fs');
 const { start } = require('repl');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
     : `5
 5
-2
-3
 4
+3
+2
 1`
 ).split('\n');
 
@@ -24,5 +24,24 @@ for(let i=0; i<t; i++){
   arr.push(num)
 
 }
-const arrangedNum = arr.sort((a,b)=> b-a)
+const arrangedNum = arr.sort((a,b)=> a-b)
 arrangedNum.forEach(each => console.log(each))
+
+
+// 2751 수 정렬하기2
+
+// 1427 소트인사이드(정답확인)
+const fs = require('fs');
+const { start } = require('repl');
+const stdin = (process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin').toString()
+    : `999998999`
+).split('\n');
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const t = input().split('').map(Number).sort((a,b)=>b-a).join('')
+console.log(t)
--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const fs = require('fs');
 const { start } = require('repl');
 const stdin = (process.platform === 'linux'
     ? fs.readFileSync('/dev/stdin').toString()
-    : `5
-5
-2
-3
-4
-1`
+    : `999998999`
 ).split('\n');
 
 const input = (() => {
@@ -15,13 +10,7 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
-const t = Number(input())
-let arr=[];
+const t = input().split('').map(Number).sort((a,b)=>b-a).join('')
+console.log(t)
 
-for(let i=0; i<t; i++){
-  let num = Number(input())
-  arr.push(num)
 
-}
-const arrangedNum = arr.sort((a,b)=> b-a)
-arrangedNum.forEach(each => console.log(each))
